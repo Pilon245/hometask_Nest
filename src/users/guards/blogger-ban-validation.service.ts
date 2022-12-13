6a47d72e72fa832d67e2bf71f5b31942682bf791
Amirhanov Said <saidderbentskij@yandex.ru>
2022-12-14 02:55:33 +0300

    Nest 16.13 TEst commit new Branch

--- src/users/guards/blogger-ban-validation.service.ts
+++ src/users/guards/blogger-ban-validation.service.ts
@@ -12,10 +12,7 @@ export class BloggerExistsRule implements ValidatorConstraintInterface {
   constructor(private blogsQueryRepository: BlogsQueryRepository) {}
   async validate(value: string) {
     try {
-      console.log('value', value);
-      console.log('this.blogsQueryRepository', this.blogsQueryRepository);
       const result = await this.blogsQueryRepository.findBlogById(value);
-      console.log(result);
       if (!result) return false;
       return true;
     } catch (e) {
