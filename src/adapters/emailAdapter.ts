b0f0765e9cbab6dd55488d0f85c7adfc3167e2b0
Amirhanov Said <saidderbentskij@yandex.ru>
2023-01-14 13:14:25 +0300

    Nest 17.13 testing all data

--- src/adapters/emailAdapter.ts
+++ src/adapters/emailAdapter.ts
@@ -16,11 +16,10 @@ export class EmailAdapter {
       to: email, // list of receivers
       subject: 'Registration', // Subject line
       text: "Hello friends, I'am five age!", // plain text body
-      html:
-        ' <h1>Thank for your registration</h1>\n' +
-        '       <p>To finish registration please follow the link below:\n' +
-        `          <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>\n` +
-        '      </p>', // html body
+      html: `<h1>Thank for your registration</h1>
+               <p>To finish registration please follow the link below:
+                  <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+              </p>`, // html body
     });
     return info;
   }
